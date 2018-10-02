412a103b0ac30ca5e08292a181f039a2d577cf95
Oganesyan Artur <arturris@mail.ru>
2018-10-02 13:04:10 -0500

    Mongoose Validation

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,16 +6,24 @@ const Product = require('../models/product');
 
 router.get('/', (req,res, next) => {
     Product.find()
+    .select('name price _id')
     .exec()
     .then(docs => {
-        console.log(docs);
-        if (docs.length >= 0) {
-            res.status(200).json(docs)
-        } else {
-            res.status(404).json({
-                message: 'No entries'
-            })
-        }
+      const response = {
+          count: docs.length,
+          products: docs.map(doc => {
+              return {
+                  name: doc.name,
+                  price: doc.price,
+                  _id: doc._id, 
+                  request: {
+                      type: 'GET',
+                      url: 'http://localhost:3000/products/' + doc._id
+                  }
+              }
+          })
+      }
+      res.status(200).json(response)
     })
     .catch(err => {
         console.log(err)
@@ -35,7 +43,15 @@ router.post('/', (req,res, next) => {
         console.log(result)
         res.status(201).json({
             message: 'Handling POST request to /products',
-            createdProduct: result
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + result._id
+                }
+            }
         })
     })
     .catch(err => {
@@ -49,11 +65,20 @@ router.post('/', (req,res, next) => {
 router.get('/:productId', (req,res,next) => {
     const id = req.params.productId;
     Product.findById(id)
+    .select('name price _id')
     .exec()
     .then(doc => {
         console.log('From database', doc);
         if (doc) {
-            res.status(200).json(doc)
+            res.status(200).json({
+                name: doc.name,
+                price: doc.price,
+                _id: doc._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + doc._id
+                }
+            })
         } else {
             res.status(404).json({message: 'No valid entry foud for provided ID'});
         }
@@ -74,7 +99,13 @@ router.patch('/:productId', (req,res,next) => {
     .exec()
     .then(result => {
         console.log(result);
-        res.status(200).json(result)
+        res.status(200).json({
+            message: 'Product Updated',
+            request: {
+                type: "GET",
+                url: 'http://localhost:3000/products/' + id
+            }
+        })
     })
     .catch(err => {
         console.log(err);
@@ -89,7 +120,14 @@ router.delete('/:productId', (req,res,next) => {
     Product.remove({_id: id})
     .exec()
     .then(result => {
-        res.status(200).json(result)
+        res.status(200).json({
+            message: 'Product deleted',
+            request: {
+                type: 'POST',
+                url: "http://localhost:3000/products",
+                body: {name: 'String', price: 'Number'}
+            }
+        })
     })
     .catch(err => {
         console.log(err);
--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const orderRoutes = require('./api/routes/orders');
 mongoose.connect('mongodb://arturris:'+process.env.MONGO_ATLAS_PW+'@node-rest-shop-shard-00-00-kedob.mongodb.net:27017,node-rest-shop-shard-00-01-kedob.mongodb.net:27017,node-rest-shop-shard-00-02-kedob.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true', {
     useNewUrlParser: true 
 })
+mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
