80e66afddaa40b302a86f76f2e5ec188e5d0f29d
Oganesyan Artur <arturris@mail.ru>
2018-10-02 12:04:02 -0500

    MongoDB & Mongoose set up

--- app.js
+++ app.js
@@ -2,22 +2,27 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://arturris:'+process.env.MONGO_ATLAS_PW+'@node-rest-shop-shard-00-00-kedob.mongodb.net:27017,node-rest-shop-shard-00-01-kedob.mongodb.net:27017,node-rest-shop-shard-00-02-kedob.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true', {
+    useNewUrlParser: true 
+})
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization '
-    );
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+      );
     if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods',
-        'PUT, POST, PATCH, DELETE, GET');
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
         return res.status(200).json({});
     }
     next();
