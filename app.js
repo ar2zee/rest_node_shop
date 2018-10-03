06f49486ec7e4ac7451e6882e26832582a5958c9
Oganesyan Artur <arturris@mail.ru>
2018-10-03 16:31:42 -0500

    image uploader

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect('mongodb://arturris:'+process.env.MONGO_ATLAS_PW+'@node-rest-sh
 mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
