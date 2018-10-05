ecad5621223bb6bbf20800745ef9461e04c084d0
Oganesyan Artur <arturris@mail.ru>
2018-10-04 20:41:00 -0500

    users signup

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
+mongoose.set('useCreateIndex', true)
 mongoose.connect('mongodb://arturris:'+process.env.MONGO_ATLAS_PW+'@node-rest-shop-shard-00-00-kedob.mongodb.net:27017,node-rest-shop-shard-00-01-kedob.mongodb.net:27017,node-rest-shop-shard-00-02-kedob.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true', {
     useNewUrlParser: true 
 })
@@ -32,6 +34,7 @@ app.use((req,res,next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req,res,next) => {
     const error = new Error('Not found');
